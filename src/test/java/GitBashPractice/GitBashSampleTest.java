1dbf0effb87399799332212bbb83c5b1bccef488
akil-s28 <solkarakil97@gmail.com>
2025-05-31 13:10:45 +0530

    msg3

---
+++ src/test/java/GitBashPractice/GitBashSampleTest.java
@@ -0,0 +1,11 @@
+package GitBashPractice;
+
+import org.testng.annotations.Test;
+
+public class GitBashSampleTest {
+	@Test
+	public void gitbashTest() {
+		System.out.println("GitBashTest");
+	}
+
+}
